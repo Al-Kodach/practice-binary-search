8c502817d3a5d5840951d5e257068522ec6e71ff
Al-Kodach <alkodach351@gmail.com>
2023-05-26 06:49:13 -0400

    b-search

--- binary-search.js
+++ binary-search.js
@@ -1,29 +1,33 @@
-function linearSearch (arr, target) {
+function linearSearch(arr, target) {
 
   // Can you solve this in one line?
+   return arr.indexOf(target);
 
 };
 
 function binarySearch(arr, target) {
 
+
   // Set integers pointing to the high and low range of possible indices
+    let lo = 0,
+      hi = arr.length;
 
-  // While high and low indices do not overlap...
+    while (lo < hi) {
+      let mid = Math.floor((lo + hi) / 2);
+      if (arr[mid] === target) {
+        return arr[mid];
+      } else if (arr[mid] < target) {
+        lo = mid + 1;
+      } else {
+        hi = mid;
+      }
 
-    // Find the midpoint between high and low indices
-
-    // Compare the target value to the midpoint value
-
-    // If the target equals the midpoint...
-      // Return the midpoint index
-    // If the target is higher than the midpoint...
-      // Move the low pointer to midpoint + 1
-    // If the target is less than the midpoint...
-      // Move the high pointer to midpoint - 1
-
-  // Return -1 if the loop exits with overlapping pointers
+    } 
+   
+    return -1
+    
+ 
 
 }
 
-
-module.exports = [linearSearch, binarySearch]
+  module.exports = [linearSearch, binarySearch];
